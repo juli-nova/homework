3b40dc95ed365a9404be8a3acdbdd76118a00270
juli-nova <57984367+juli-nova@users.noreply.github.com>
2019-12-10 22:17:49 +0200

    Create 12.js

---
+++ 11/12.js
@@ -0,0 +1,15 @@
+
+//https://www.codewars.com/kata/training-js-number-13-number-object-and-its-properties
+
+
+switch (true) {
+    case (n==Number.MAX_VALUE): return "Input number is Number.MAX_VALUE";
+    case (n==Number.POSITIVE_INFINITY): return "Input number is Number.POSITIVE_INFINITY";
+    case (n==Number.MIN_VALUE): return "Input number is Number.MIN_VALUE";
+    case (n==Number.NEGATIVE_INFINITY): return "Input number is Number.NEGATIVE_INFINITY";
+    case (isNaN(n)): return "Input number is Number.NaN";
+    default: return "Input number is " + n;
+  }
+}
+
+
