bdaa58dc431339d664b7cb139f8fe721703570c9
juli-nova <57984367+juli-nova@users.noreply.github.com>
2019-12-11 15:35:50 +0200

    Update 12.js

--- 11/12.js
+++ 11/12.js
@@ -1,8 +1,8 @@
 
 //https://www.codewars.com/kata/training-js-number-13-number-object-and-its-properties
 
-
-switch (true) {
+function whatNumberIsIt(n){
+  switch (true) {
     case (n==Number.MAX_VALUE): return "Input number is Number.MAX_VALUE";
     case (n==Number.POSITIVE_INFINITY): return "Input number is Number.POSITIVE_INFINITY";
     case (n==Number.MIN_VALUE): return "Input number is Number.MIN_VALUE";
@@ -10,6 +10,41 @@ switch (true) {
     case (isNaN(n)): return "Input number is Number.NaN";
     default: return "Input number is " + n;
   }
-}
+    
+    
+    
+    
+ //https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript 
+    
+  const giveMeFive = obj => {
+  const result = []
 
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (key.length === 5) result.push(key)
+      if (obj[key].length === 5) result.push(obj[key])
+    }
+  }
+
+  return result
+}  
+    
+  
+  //https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+  
+  function buildFun(n){
+	var res = []
+	for (let i = 0; i< n; i++){
+		res.push(function(){
+			return i
+		})
+	}
+	return res
+}
+    
+    
+    //https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+    
+    
+    
 
